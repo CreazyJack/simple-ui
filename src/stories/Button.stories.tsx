e8501f9c22079f856ebbfe4f4d738c6ea59b51a1
CreazyJack <51393413+CreazyJack@users.noreply.github.com>
2021-05-10 16:22:39 +0800

    1

--- src/stories/Button.stories.tsx
+++ src/stories/Button.stories.tsx
@@ -2,7 +2,7 @@
  * @Description: 未添加描述
  * @Date: 2021-05-08 15:56:47
  * @LastEditors: JackyChou
- * @LastEditTime: 2021-05-10 15:37:31
+ * @LastEditTime: 2021-05-10 15:53:31
  */
 
 import { Story, Meta } from '@storybook/react';
@@ -10,7 +10,7 @@ import { action } from '@storybook/addon-actions';
 import Button, { ButtonProps } from '../components/button/button';
 import '../styles/index.scss';
 
-const Template: Story<ButtonProps> = (args) => <Button {...args}>{args.type}</Button>;
+const Template: Story<ButtonProps> = (args) => <Button {...args}>{args.type || 'null'}</Button>;
 
 const Primary = Template.bind({});
 Primary.args = {
@@ -19,22 +19,18 @@ Primary.args = {
   danger: false,
 };
 
-export { Primary };
+const 不同大小的按钮: React.FC = () => (
+  <>
+    <Button size="small">small</Button>
+    <Button size="middle">middle</Button>
+    <Button size="large">large</Button>
+  </>
+);
+
+export { Primary, 不同大小的按钮 };
 
 export default {
   title: 'Example/Button',
   component: Button,
-  argTypes: {
-    type: {
-      control: {
-        type: 'radio',
-        options: ['primary', 'link', 'default'],
-      },
-    },
-    danger: {
-      control: {
-        type: 'boolean',
-      },
-    },
-  },
+  argTypes: {},
 } as Meta;
