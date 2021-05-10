65c5f9111ef39face787b61263574d3f8093e20b
CreazyJack <51393413+CreazyJack@users.noreply.github.com>
2021-05-10 20:01:39 +0800

    1

--- src/stories/Button.stories.tsx
+++ src/stories/Button.stories.tsx
@@ -2,9 +2,10 @@
  * @Description: 未添加描述
  * @Date: 2021-05-08 15:56:47
  * @LastEditors: JackyChou
- * @LastEditTime: 2021-05-10 15:53:31
+ * @LastEditTime: 2021-05-10 17:32:24
  */
 
+import { useState } from 'react';
 import { Story, Meta } from '@storybook/react';
 import { action } from '@storybook/addon-actions';
 import Button, { ButtonProps } from '../components/button/button';
@@ -19,13 +20,19 @@ Primary.args = {
   danger: false,
 };
 
-const 不同大小的按钮: React.FC = () => (
-  <>
-    <Button size="small">small</Button>
-    <Button size="middle">middle</Button>
-    <Button size="large">large</Button>
-  </>
-);
+const 不同大小的按钮: React.FC = () => {
+  const [num, setNum] = useState(0);
+  const add = () => setNum(num + 1);
+  return (
+    <>
+      <Button size="small" onClick={add}>
+        small
+      </Button>
+      <Button size="middle">middle</Button>
+      <Button size="large">large</Button>
+    </>
+  );
+};
 
 export { Primary, 不同大小的按钮 };
 
