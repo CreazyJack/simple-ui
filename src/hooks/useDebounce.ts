1026e081950f8566f0695a44e42fca3bb14af8dd
CreazyJack <51393413+CreazyJack@users.noreply.github.com>
2021-05-10 21:02:54 +0800

    1

--- src/hooks/useDebounce.ts
+++ src/hooks/useDebounce.ts
@@ -2,7 +2,7 @@
  * @Description: debounce hook
  * @Date: 2021-05-10 16:42:45
  * @LastEditors: JackyChou
- * @LastEditTime: 2021-05-10 20:00:35
+ * @LastEditTime: 2021-05-10 21:02:48
  */
 
 import { useCallback, useEffect, useRef } from 'react';
@@ -15,12 +15,8 @@ const useDebounce: UseDebounceType = (fn, delay = 300) => {
   const { current } = useRef<{ fn: Fn; timer: NodeJS.Timeout | null }>({ fn, timer: null });
   const debounce = useCallback(
     (...args) => {
-      if (current.timer) {
-        clearTimeout(current.timer);
-      }
-      current.timer = setTimeout(() => {
-        current.fn(...args);
-      }, delay);
+      if (current.timer) clearTimeout(current.timer);
+      current.timer = setTimeout(() => current.fn(...args), delay);
     },
     [current, delay],
   );
